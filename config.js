05e0e3f6761155c43ec1ac2a97f8b16ab2336496
Hoang Tuan Anh <tuanballbooo@gmail.com>
2024-02-07 23:41:38 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Hà Anh ơi, tớ chỉ muốn nói là cậu xinh lắm ấyyy 😉 😘',
+    introDesc: `Ấn nút dưới đi
+                      `,
+    btnIntro: 'Click Me',
+    title: 'Hà Anh ơi, lì xì cho Tuấn Anh nhóe =)) ',
+    desc: 'Không trả lời mà thoát thì kiếp này làm ny tớ =))) ',
+    btnYes: 'Đc thuiii <33',
+    btnNo: 'Không, đừng mơ :))',
+    question:'K trốn đc đâu ạ, lì xì ikkk '+'\nStk: XXXXXXXXXX',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
